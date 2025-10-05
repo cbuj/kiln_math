6d2e743fe4b4b566f80713af61f47eeb65e08eca
Corbin Bujnowski <mrnibroc@gmail.com>
2025-10-05 13:28:24 -0700

    added traits for matrix4

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,10 +1,12 @@
 use std::{ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Rem, RemAssign, Sub, SubAssign}, sync::Arc};
 use num_traits::{Float, One, Zero};
 use crate::vector::{Vector2, Vector3, Vector4};
+
 //--------
 // Matrix2
 //--------
 
+#[repr(C)]
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Matrix2<T> {
     pub c1r1: T, pub c1r2: T,
@@ -168,6 +170,7 @@ impl<T> RemAssign for Matrix2<T> where T: RemAssign<T> {
 // Matrix3
 //--------
 
+#[repr(C)]
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Matrix3<T> {
     pub c1r1: T, pub c1r2: T, pub c1r3: T,
@@ -395,6 +398,7 @@ impl<T> RemAssign for Matrix3<T> where T: RemAssign<T> {
 // Matrix4
 //--------
 
+#[repr(C)]
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Matrix4<T> {
     pub c1r1: T, pub c1r2: T, pub c1r3: T, pub c1r4: T,
@@ -416,6 +420,292 @@ impl<T> Matrix4<T> {
             c3r1, c3r2, c3r3, c3r4,
             c4r1, c4r2, c4r3, c4r4
         }
+    }
+}
+
+impl<T> Matrix4<T> where T: Zero {
+    pub fn zero() -> Self {
+        Self {
+            c1r1: T::zero(),
+            c1r2: T::zero(),
+            c1r3: T::zero(),
+            c1r4: T::zero(),
+            c2r1: T::zero(),
+            c2r2: T::zero(),
+            c2r3: T::zero(),
+            c2r4: T::zero(),
+            c3r1: T::zero(),
+            c3r2: T::zero(),
+            c3r3: T::zero(),
+            c3r4: T::zero(),
+            c4r1: T::zero(),
+            c4r2: T::zero(),
+            c4r3: T::zero(),
+            c4r4: T::zero()
+        }
+    }
+}
+
+impl<T> Matrix4<T> where T: One {
+    pub fn one() -> Self {
+        Self {
+            c1r1: T::one(),
+            c1r2: T::one(),
+            c1r3: T::one(),
+            c1r4: T::one(),
+            c2r1: T::one(),
+            c2r2: T::one(),
+            c2r3: T::one(),
+            c2r4: T::one(),
+            c3r1: T::one(),
+            c3r2: T::one(),
+            c3r3: T::one(),
+            c3r4: T::one(),
+            c4r1: T::one(),
+            c4r2: T::one(),
+            c4r3: T::one(),
+            c4r4: T::one()           
+        }
+    }
+}
+
+// Matrix4 Addition
+
+impl<T> Add for Matrix4<T> where T: Add<Output = T> {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            c1r1: self.c1r1 + rhs.c1r1,
+            c1r2: self.c1r2 + rhs.c1r2,
+            c1r3: self.c1r3 + rhs.c1r3,
+            c1r4: self.c1r4 + rhs.c1r4,
+            c2r1: self.c2r1 + rhs.c2r1,
+            c2r2: self.c2r2 + rhs.c2r2,
+            c2r3: self.c2r3 + rhs.c2r3,
+            c2r4: self.c2r4 + rhs.c2r4,
+            c3r1: self.c3r1 + rhs.c3r1,
+            c3r2: self.c3r2 + rhs.c3r2,
+            c3r3: self.c3r3 + rhs.c3r3,
+            c3r4: self.c3r4 + rhs.c3r4,
+            c4r1: self.c4r1 + rhs.c4r1,
+            c4r2: self.c4r2 + rhs.c4r2,
+            c4r3: self.c4r3 + rhs.c4r3,
+            c4r4: self.c4r4 + rhs.c4r4
+        }
+    }
+}
+
+impl<T> AddAssign for Matrix4<T> where T: AddAssign<T> {
+    fn add_assign(&mut self, rhs: Self) {
+        self.c1r1 += rhs.c1r1;
+        self.c1r2 += rhs.c1r2;
+        self.c1r3 += rhs.c1r3;
+        self.c1r4 += rhs.c1r4;
+        self.c2r1 += rhs.c2r1;
+        self.c2r2 += rhs.c2r2;
+        self.c2r3 += rhs.c2r3;
+        self.c2r4 += rhs.c2r4;
+        self.c3r1 += rhs.c3r1;
+        self.c3r2 += rhs.c3r2;
+        self.c3r3 += rhs.c3r3;
+        self.c3r4 += rhs.c3r4;
+        self.c4r1 += rhs.c4r1;
+        self.c4r2 += rhs.c4r2;
+        self.c4r3 += rhs.c4r3;
+        self.c4r4 += rhs.c4r4;
+    }
+}
+
+// Matrix4 Subtraction
+
+impl<T> Sub for Matrix4<T> where T: Sub<Output = T> {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self {
+            c1r1: self.c1r1 - rhs.c1r1,
+            c1r2: self.c1r2 - rhs.c1r2,
+            c1r3: self.c1r3 - rhs.c1r3,
+            c1r4: self.c1r4 - rhs.c1r4,
+            c2r1: self.c2r1 - rhs.c2r1,
+            c2r2: self.c2r2 - rhs.c2r2,
+            c2r3: self.c2r3 - rhs.c2r3,
+            c2r4: self.c2r4 - rhs.c2r4,
+            c3r1: self.c3r1 - rhs.c3r1,
+            c3r2: self.c3r2 - rhs.c3r2,
+            c3r3: self.c3r3 - rhs.c3r3,
+            c3r4: self.c3r4 - rhs.c3r4,
+            c4r1: self.c4r1 - rhs.c4r1,
+            c4r2: self.c4r2 - rhs.c4r2,
+            c4r3: self.c4r3 - rhs.c4r3,
+            c4r4: self.c4r4 - rhs.c4r4
+        }
+    }
+}
+
+impl<T> SubAssign for Matrix4<T> where T: SubAssign<T> {
+    fn sub_assign(&mut self, rhs: Self) {
+        self.c1r1 -= rhs.c1r1;
+        self.c1r2 -= rhs.c1r2;
+        self.c1r3 -= rhs.c1r3;
+        self.c1r4 -= rhs.c1r4;
+        self.c2r1 -= rhs.c2r1;
+        self.c2r1 -= rhs.c2r2;
+        self.c2r3 -= rhs.c2r3;
+        self.c2r4 -= rhs.c2r4;
+        self.c3r1 -= rhs.c3r1;
+        self.c3r2 -= rhs.c3r2;
+        self.c3r3 -= rhs.c3r3;
+        self.c3r4 -= rhs.c3r4;
+        self.c4r1 -= rhs.c4r1;
+        self.c4r2 -= rhs.c4r2;
+        self.c4r3 -= rhs.c4r3;
+        self.c4r4 -= rhs.c4r4;
+    }
+}
+
+// Matrix4 Multiplication
+
+impl<T> Mul for Matrix4<T> where T: Mul<Output = T> {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self {
+            c1r1: self.c1r1 * rhs.c1r1,
+            c1r2: self.c1r2 * rhs.c1r2,
+            c1r3: self.c1r3 * rhs.c1r3,
+            c1r4: self.c1r4 * rhs.c1r4,
+            c2r1: self.c2r1 * rhs.c2r1,
+            c2r2: self.c2r2 * rhs.c2r2,
+            c2r3: self.c2r3 * rhs.c2r3,
+            c2r4: self.c2r4 * rhs.c2r4,
+            c3r1: self.c3r1 * rhs.c3r1,
+            c3r2: self.c3r2 * rhs.c3r2,
+            c3r3: self.c3r3 * rhs.c3r3,
+            c3r4: self.c3r4 * rhs.c3r4,
+            c4r1: self.c4r1 * rhs.c4r1,
+            c4r2: self.c4r2 * rhs.c4r2,
+            c4r3: self.c4r3 * rhs.c4r3,
+            c4r4: self.c4r4 * rhs.c4r4
+        }
+    }
+}
+
+impl<T> MulAssign for Matrix4<T> where T: MulAssign<T> {
+    fn mul_assign(&mut self, rhs: Self) {
+        self.c1r1 *= rhs.c1r1;
+        self.c1r2 *= rhs.c1r2;
+        self.c1r3 *= rhs.c1r3;
+        self.c1r4 *= rhs.c1r4;
+        self.c2r1 *= rhs.c2r1;
+        self.c2r2 *= rhs.c2r2;
+        self.c2r3 *= rhs.c2r3;
+        self.c2r4 *= rhs.c2r4;
+        self.c3r1 *= rhs.c3r1;
+        self.c3r2 *= rhs.c3r2;
+        self.c3r3 *= rhs.c3r3;
+        self.c3r4 *= rhs.c3r4;
+        self.c4r1 *= rhs.c4r1;
+        self.c4r2 *= rhs.c4r2;
+        self.c4r3 *= rhs.c4r3;
+        self.c4r4 *= rhs.c4r4;
+    }
+}
+
+// Matrix4 Division
+
+impl<T> Div for Matrix4<T> where T: Div<Output = T> {
+    type Output = Self;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        Self {
+            c1r1: self.c1r1 / rhs.c1r1,
+            c1r2: self.c1r2 / rhs.c1r2,
+            c1r3: self.c1r3 / rhs.c1r3,
+            c1r4: self.c1r4 / rhs.c1r4,
+            c2r1: self.c2r1 / rhs.c2r1,
+            c2r2: self.c2r2 / rhs.c2r2,
+            c2r3: self.c2r3 / rhs.c2r3,
+            c2r4: self.c2r4 / rhs.c2r4,
+            c3r1: self.c3r1 / rhs.c3r1,
+            c3r2: self.c3r2 / rhs.c3r2,
+            c3r3: self.c3r3 / rhs.c3r3,
+            c3r4: self.c3r4 / rhs.c3r4,
+            c4r1: self.c4r1 / rhs.c4r1,
+            c4r2: self.c4r2 / rhs.c4r2,
+            c4r3: self.c4r3 / rhs.c4r3,
+            c4r4: self.c4r4 / rhs.c4r4
+        }
+    }
+}
+
+impl<T> DivAssign for Matrix4<T> where T: DivAssign<T> {
+    fn div_assign(&mut self, rhs: Self) {
+        self.c1r1 /= rhs.c1r1;
+        self.c1r2 /= rhs.c1r2;
+        self.c1r3 /= rhs.c1r3;
+        self.c1r4 /= rhs.c1r4;
+        self.c2r1 /= rhs.c2r1;
+        self.c2r2 /= rhs.c2r2;
+        self.c2r3 /= rhs.c2r3;
+        self.c2r4 /= rhs.c2r4;
+        self.c3r1 /= rhs.c3r1;
+        self.c3r2 /= rhs.c3r2;
+        self.c3r3 /= rhs.c3r3;
+        self.c3r4 /= rhs.c3r4;
+        self.c4r1 /= rhs.c4r1;
+        self.c4r2 /= rhs.c4r2;
+        self.c4r3 /= rhs.c4r3;
+        self.c4r4 /= rhs.c4r4;
+    }
+}
+
+// Matrix4 Remainder
+
+impl<T> Rem for Matrix4<T> where T: Rem<Output = T> {
+    type Output = Self;
+
+    fn rem(self, rhs: Self) -> Self::Output {
+        Self {
+            c1r1: self.c1r1 % rhs.c1r1,
+            c1r2: self.c1r2 % rhs.c1r2,
+            c1r3: self.c1r3 % rhs.c1r3,
+            c1r4: self.c1r4 % rhs.c1r4,
+            c2r1: self.c2r1 % rhs.c2r1,
+            c2r2: self.c2r2 % rhs.c2r2,
+            c2r3: self.c2r3 % rhs.c2r3,
+            c2r4: self.c2r4 % rhs.c2r4,
+            c3r1: self.c3r1 % rhs.c3r1,
+            c3r2: self.c3r2 % rhs.c3r2,
+            c3r3: self.c3r3 % rhs.c3r3,
+            c3r4: self.c3r4 % rhs.c3r4,
+            c4r1: self.c4r1 % rhs.c4r1,
+            c4r2: self.c4r2 % rhs.c4r2,
+            c4r3: self.c4r3 % rhs.c4r3,
+            c4r4: self.c4r4 % rhs.c4r4
+        }
+    }
+}
+
+impl<T> RemAssign for Matrix4<T> where T: RemAssign<T> {
+    fn rem_assign(&mut self, rhs: Self) {
+        self.c1r1 %= rhs.c1r1;
+        self.c1r2 %= rhs.c1r2;
+        self.c1r3 %= rhs.c1r3;
+        self.c1r4 %= rhs.c1r4;
+        self.c2r1 %= rhs.c2r1;
+        self.c2r2 %= rhs.c2r2;
+        self.c2r3 %= rhs.c2r3;
+        self.c2r4 %= rhs.c2r4;
+        self.c3r1 %= rhs.c3r1;
+        self.c3r2 %= rhs.c3r2;
+        self.c3r3 %= rhs.c3r3;
+        self.c3r4 %= rhs.c3r4;
+        self.c4r1 %= rhs.c4r1;
+        self.c4r2 %= rhs.c4r2;
+        self.c4r3 %= rhs.c4r3;
+        self.c4r4 %= rhs.c4r4;
     }
 }
 
