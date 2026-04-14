9307bee0246489fcb79373000d94e388a12393d3
Corbin Bujnowski <mrnibroc@gmail.com>
2026-04-14 12:15:04 -0600

    Blank slate for project

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,2 @@
-pub mod matrix;
-pub mod point;
-pub mod vector;
-pub mod quaternion;
-
-use std::fmt::Debug;
-use std::{marker::PhantomData};
-
-//------------
-// EulerAngles
-//------------
-
-#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct EulerAngles<T, B> {
-    pub a: T,
-    pub b: T,
-    pub c: T,
-    pub marker: PhantomData<B>
-}
-
 
 
